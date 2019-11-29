fbc5004e097709ac8dc530292951a38ba61d6443
kozin <dfight63@gmail.com>
2019-11-29 09:36:30 +0900

    이벤트 View 단 일단 데이터 넘기는 페이지만 만들고 디자인 오면 입히는 형태로 진행

---
+++ application/views/Event_view.php
@@ -0,0 +1,30 @@
+<div>
+<form action="">
+    <table>      
+        <tbody>
+            <tr>
+                <th>반려견 이름</th>
+                <td></td>
+                <th>반려견 생년월일</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>가입자이름</th>
+                <td></td>
+                <th>가입자 휴대폰번호</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>가입보험사</th>
+                <td></td>
+                <th>가입증권번호</th>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="4">개인정보수집 동의합니다. <input type="checkbox"></td>
+            </tr>               
+        </tbody>
+    </table>
+    <div><button type="button" id="eventsub">이벤트 참여하기</button></div>
+</form>
+</div>
